00a809fb117375f4173c85a1b3b0b48f4d7b8c03
Edelmiro Schnack Aragón <56047945+Edelmeyder@users.noreply.github.com>
2021-09-19 19:26:08 -0300

    Update password_manager.c
    Arreglados algunos problemas.

--- src/password_manager.c
+++ src/password_manager.c
@@ -1,3 +1,8 @@
+/*
+Autor Edelmiro Schnack AragÃ³n.
+19/07/2021
+*/
+
 #include "password_manager.h"
 
 typedef enum {
@@ -13,13 +18,13 @@ static uint8_t buffer_c[4];
 static uint32_t tiempo = 0; //tiempo en estado, se usa para saber la primera vez que se entra a un estado y para algunas temporizaciones
 uint8_t key;
 
-uint8_t autenticar(void); //esta función compara la contraseña ingresada con la contraseña actual, retorna 1 si son iguales, 0 cc
+uint8_t autenticar(void); //esta funci?n compara la contrase?a ingresada con la contrase?a actual, retorna 1 si son iguales, 0 cc
 
 void PASSWORD_MANAGER_init(void){
 	estado = CERRADO;
 }
 
-void PASSWORD_MANAGER_Update(void){
+void PASSWORD_MANAGER_update(void){
 	switch (estado) {
 		case CERRADO:
 			if (tiempo == 0) {
@@ -29,7 +34,7 @@ void PASSWORD_MANAGER_Update(void){
 				tiempo = 1;
 			}
 			if (Keyboard_Update(&key)) {
-				//si se detecta un número se pasa a abriendo
+				//si se detecta un n?mero se pasa a abriendo
 				if (key >= '0' && key <= '9') {
 					buffer_c[0] = key;
 					estado = ABRIENDO;
@@ -40,7 +45,7 @@ void PASSWORD_MANAGER_Update(void){
 		case ABRIENDO:
 			switch (estado_a) {
 				case PRIMERO:
-					//el primer estado de abriendo solo bora el diplay y escribe un * ya que la tecla ya se guardó
+					//el primer estado de abriendo solo bora el diplay y escribe un * ya que la tecla ya se guardÃ³
 					LCD_goToXY(4, 1);
 					LCD_sendString((uint8_t*) "       ", 7);
 					LCD_goToXY(6, 1);
@@ -85,7 +90,7 @@ void PASSWORD_MANAGER_Update(void){
 				LCD_goToXY(4, 1);
 				LCD_sendString((uint8_t*) "ABIERTO ", 8);
 			}
-			if (tiempo == 24) { //24 iteraciones en el estado son 3 segundos
+			if (tiempo == 50) { //50 iteraciones en el estado son .5 segundos
 				estado = CERRADO;
 				tiempo = 0;
 			}
@@ -95,7 +100,7 @@ void PASSWORD_MANAGER_Update(void){
 				LCD_goToXY(4, 1);
 				LCD_sendString((uint8_t*) "DENEGADO", 8);
 			}
-			if (tiempo == 16) { //16 iteraciones en el estado son 2 segundos
+			if (tiempo == 20) { //2 iteraciones en el estado son .2 segundos
 				estado = CERRADO;
 				tiempo = 0;
 			}
@@ -104,7 +109,8 @@ void PASSWORD_MANAGER_Update(void){
 }
 
 uint8_t autenticar(void) {
-	for (uint8_t i = 0; i < 4; i++){
+	uint8_t i;
+	for (i = 0; i < 4; i++){
 		if (PASS[i] != buffer_c[i]) return 0;
 	}
 	return 1;
